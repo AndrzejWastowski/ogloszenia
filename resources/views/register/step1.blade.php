6b6ddda17ea1c1521646cf320e3ba1942d7ced6e
Andrzej Wastowski <andrzej@wastowski.pl>
2020-12-09 22:21:55 +0100

    dodany testowy multistep - register

---
+++ resources/views/register/step1.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Step 1</h1>
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+     <form action="/register1" method="POST">
+        @csrf
+     <input type="text" name="name" class="form-controll" placeholder="Enter name" value="{{ session()->get('register.name') }}">
+         <button type="submit" class="btn btn-primary">Continue</button>
+     </form>
+@endsection 
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,16 @@ Auth::routes();
 Route::get('/start', [App\Http\Controllers\HomeController::class, 'index'])->name('start');
 Route::get('/home/tablica', [App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::group(['namespace' => 'App\Http\Controllers'], function(){
+    Route::get('/register1', 'RegisterController@createStep1')->name('signup');
+    Route::post('/register1', 'RegisterController@PostcreateStep1');
+    Route::get('/register2', 'RegisterController@createStep2')->name('step2');
+    Route::post('/register2', 'RegisterController@PostcreateStep2');
+    Route::get('/register3', 'RegisterController@createStep3');
+    Route::post('/register3', 'RegisterController@PostcreateStep3');
+    Route::post('/remove-image', 'RegisterController@removeImage');
+    Route::post('/store', 'RegisterController@store');
+    Route::get('/data', 'RegisterController@index');
+});
