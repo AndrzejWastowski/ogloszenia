669d688e8c4cb1ad92264fa3bac66d96ef31ba53
Andrzej Wastowski <andrzej@wastowski.pl>
2021-04-26 15:31:55 +0200

    poprawki w start szablonach

--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -5,11 +5,9 @@
     @include('start.tab_menu')
     @include('start.popular_category')            
     @include('start.4reklamy_gora')
-    @include('start.promo')
-
-    <hr><br><hr><hr><hr><hr><hr><hr><hr><hr>
-    @include('start.4reklamy_dol')
-    @include('start.ads_slider')
+    <!-- @include('start.promo') 
+    @include('start.4reklamy_dol')   
+    @include('start.ads_slider')  -->
     @include('start.estates_slider')
 </main>
 @endsection
