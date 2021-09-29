378fb593d3960a599595c308d3aa23c44974f203
Andrzej Wastowski <andrzej@wastowski.pl>
2021-09-29 16:08:33 +0200

    poprawki w dziale motoryzacyjnym

--- resources/views/home/add/automotive/type.blade.php
+++ resources/views/home/add/automotive/type.blade.php
@@ -8,8 +8,19 @@
         <li><a href="{{ route('cars_content') }}">Samochody Osobowe</a></li>       
         <li>Motocykle i Quady</li>
         <li>Sprzęt rolniczy</li>
+        <li>Busy i autobusy</li>
         <li>Pojazdy budowlane</li>
+        <li>Dostawcze</li>
+        <li>Ciężarowe</li>
+        <li>Ciągniki Siodłowe</li>
+        <li>Rolnicze</li>
+        <li>Przyczepy i naczepy</li>
+        <li>Busy i autobusy</li>
+
+
 </div> 
+
+
 
 @endsection
 
