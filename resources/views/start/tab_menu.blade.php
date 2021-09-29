6405f26197fdd3bc592ba2b7887b2f4be9dd4cc8
Andrzej Wastowski <andrzej@wastowski.pl>
2021-09-29 16:20:55 +0200

    przebudowa zakladek tab na startowej

--- resources/views/start/tab_menu.blade.php
+++ resources/views/start/tab_menu.blade.php
@@ -1,39 +1,58 @@
 <div class="container mb-3">
-<nav>
-	<div class="nav nav-tabs" id="nav-tab" role="tablist">
-		<button class="nav-link active  id="nav-smal-ads" data-bs-toggle="tab" data-bs-target="#nav-small-ads" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Ogłoszenia Drobne</button>
-		<button class="nav-link" id="nav-estates-tab" data-bs-toggle="tab" data-bs-target="#nav-estates" type="button" role="tab" aria-controls="nav-estates" aria-selected="false">Nieruchomości</button>
-		<button class="nav-link" id="nav-job-tab" data-bs-toggle="tab" data-bs-target="#nav-job" type="button" role="tab" aria-controls="nav-job" aria-selected="false">Praca</button>
-		<button class="nav-link" id="nav-job-automoto" data-bs-toggle="tab" data-bs-target="#nav-automoto" type="button" role="tab" aria-controls="nav-automoto" aria-selected="false">Motoryzacja</button>
-	</div>
-</nav>
-<div class="tab-content" id="nav-tabContent">
-	<div class="tab-pane fade show active " id="nav-small-ads" role="tabpanel" aria-labelledby="nav-smal-ads">
-		<div class="card">
-			<div class="card-body ">Ołoszenia drobne - kategorie</div>
+	<nav>
+		<div class="nav nav-tabs" id="nav-tab" role="tablist">
+			<button class="nav-link active  id="nav-smal-ads" data-bs-toggle="tab" data-bs-target="#nav-small-ads" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Ogłoszenia Drobne</button>
+			<button class="nav-link" id="nav-estates-tab" data-bs-toggle="tab" data-bs-target="#nav-estates" type="button" role="tab" aria-controls="nav-estates" aria-selected="false">Nieruchomości</button>
+			<button class="nav-link" id="nav-services-tab" data-bs-toggle="tab" data-bs-target="#nav-services" type="button" role="tab" aria-controls="nav-services" aria-selected="false">Praca</button>
+			<button class="nav-link" id="nav-job-tab" data-bs-toggle="tab" data-bs-target="#nav-job" type="button" role="tab" aria-controls="nav-job" aria-selected="false">Praca</button>
+			<button class="nav-link" id="nav-automoto-tab" data-bs-toggle="tab" data-bs-target="#nav-automoto" type="button" role="tab" aria-controls="nav-automoto" aria-selected="false">Motoryzacja</button>
+			<button class="nav-link" id="nav-agro-tab" data-bs-toggle="tab" data-bs-target="#nav-agro" type="button" role="tab" aria-controls="nav-agro" aria-selected="false">Rolnictwo</button>
 		</div>
-	</div>
-	<div class="tab-pane fade" id="nav-estates" role="tabpanel" aria-labelledby="nav-estates-tab">  
-		<div class="card">
-			<div class="card-body">Nieruchomości - kategorie</div>
-		</div>
-	</div>
-	<div class="tab-pane fade" id="nav-job" role="tabpanel" aria-labelledby="nav-job-tab">
-		<div class="card">
-			<div class="card-body">Praca- kategorie</div>
-		</div>
-	</div>
-	<div class="tab-pane fade" id="nav-automoto" role="tabpanel" aria-labelledby="nav-automoto-tab">
-		<div class="card">
-			<div class="card-body">Motoryzacja 
-				<a href="motoryzacja/samochody_osobowe">Samochody osobowe</a>
-				<a href="motoryzacja/autobus">Busy / autobusy</a>
-				<a href="motoryzacja/ciezarowe">Samochody dostawcze i ciężarowe</a>
-				<a href="motoryzacja/motocykle">Motocykle / quady</a>
-				<a href="motoryzacja/rolnicze">Traktory / Sprzęt rolniczy</a>				
-
+	</nav>
+	<div class="tab-content" id="nav-tabContent">
+		<div class="tab-pane fade show active " id="nav-small-ads" role="tabpanel" aria-labelledby="nav-smal-ads">
+			<div class="card">
+				<div class="card-body ">Ołoszenia drobne - kategorie</div>
 			</div>
 		</div>
+		<div class="tab-pane fade" id="nav-estates" role="tabpanel" aria-labelledby="nav-estates-tab">  
+			<div class="card">
+				<div class="card-body">Nieruchomości - kategorie</div>
+			</div>
+		</div>
+		<div class="tab-pane fade" id="nav-services" role="tabpanel" aria-labelledby="nav-services-tab">
+			<div class="card">
+				<div class="card-body">Praca- kategorie</div>
+			</div>
+		</div>
+		<div class="tab-pane fade" id="nav-job" role="tabpanel" aria-labelledby="nav-job-tab">
+			<div class="card">
+				<div class="card-body">Praca - kategorie</div>
+			</div>
+		</div>
+		<div class="tab-pane fade" id="nav-automoto" role="tabpanel" aria-labelledby="nav-automoto-tab">
+			<div class="card">
+				<div class="card-body">Motoryzacja 
+					<li><a href="motoryzacja/samochody_osobowe">Samochody osobowe</a></li>
+					<li><a href="motoryzacja/autobus">Busy / autobusy</a></li>
+					<li><a href="motoryzacja/ciezarowe">Samochody dostawcze i ciężarowe</a></li>
+					<li><a href="motoryzacja/motocykle">Motocykle / quady</a></li>
+					<li><a href="motoryzacja/rolnicze">Traktory / Sprzęt rolniczy</a></li>
+
+				</div>
+			</div>
+		</div>
+		<div class="tab-pane fade" id="nav-agro" role="tabpanel" aria-labelledby="nav-agro-tab">
+			<div class="card">
+				<div class="card-body">
+					Rolnictwo
+					<ul>
+						<li>Ciągniki / kombajny</li>
+						<li>Przyczepy i naczepy</li>
+						<li>Skup / sprzedaż zwierząt</li>
+						<li>Skup / sprzedaż roślin</li>
+					</div>
+			</div>
 		</div>
 	</div>  
 </div>
