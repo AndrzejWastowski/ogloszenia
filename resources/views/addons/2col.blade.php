4c3f6977b7f0db6ec22d3eae428d7ce1e6c6892e
Andrzej Wastowski <andrzej@wastowski.pl>
2021-09-30 13:28:10 +0200

    dodane przestawianie kolumn na mobilu subkategorie

--- resources/views/addons/2col.blade.php
+++ resources/views/addons/2col.blade.php
@@ -1,8 +1,8 @@
 <section id="adverts_2col" >
 
-        <div class="card"><img class="img-fluid" src="{{ $storage->url('reklama/baner_350x250.jpg') }}"></div>
-        <div class="card"><img class="img-fluid" src="{{ $storage->url('reklama/baner_350x250.jpg') }}"></div>
-        <div class="card"><img class="img-fluid" src="{{ $storage->url('reklama/baner_350x250.jpg') }}"></div>
-        <div class="card"><img class="img-fluid" src="{{ $storage->url('reklama/baner_350x250.jpg') }}"></div>
+        <div class="card mb-2"><img class="img-fluid" src="{{ $storage->url('addons/duzy_prostokat_336_280.jpg') }}"></div>
+        <div class="card mb-2" ><img class="img-fluid" src="{{ $storage->url('addons/duzy_prostokat_336_280.jpg') }}"></div>
+        <div class="card mb-2"><img class="img-fluid" src="{{ $storage->url('addons/duzy_prostokat_336_280.jpg') }}"></div>
+        <div class="card mb-2"><img class="img-fluid" src="{{ $storage->url('addons/duzy_prostokat_336_280.jpg') }}"></div>
     
 </section>
