b04ff9c422766ce684528421141b2673f2c94921
Andrzej Wastowski <andrzej@wastowski.pl>
2021-09-30 13:21:14 +0200

    dodane przestawianie kolumn na mobilu

--- resources/views/smallAds/Content.blade.php
+++ resources/views/smallAds/Content.blade.php
@@ -2,11 +2,9 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container  mb-3">
 <div class="row">
-    
-
-    <div class="col-9">
+    <div class="col-md-9">
         @foreach($contents as $content)  
         
         <nav class="card p-3 pb-0 mb-3 bg-white" aria-label="breadcrumb">
@@ -18,14 +16,13 @@
                 <li class="breadcrumb-item active"><a href="/drobne/{{ $content->SmallAdsCategories->link }}/{{ $content->SmallAdsSubCategories->link }}/{{ $content->id }}">{{ $content->name }}</a></li>
             </ol>
         </nav>
-        <div class="card m-1">
-        <div class="card-body p-3" style="background-color: {{ $content->highlighted }};">  
-            <div class="row mb-0 pb-0">
-                <div class="col-xl-8"><h5 class="card-title"> <strong>{{ $content->name }} </strong></h5></div>
-            </div>
+
+        <div class="card">
+            <div class="card-body p-3" style="background-color: {{ $content->highlighted }};">          
+            <h5 class="card-title"> <strong>{{ $content->name }} </strong></h5>        
             <div class="card-text black-text">                
                 <div class="row">
-                    <div class="col-9">
+                    <div class="col-md-8">
                         <div id="carousel_{{ $content->id }}" class="carousel slide" data-bs-ride="carousel">
                             <div class="carousel-inner">
 
@@ -38,7 +35,7 @@
 
                                 @foreach ($content->photos as $photo)
                                     @if ($active==null) @php($active = 'active') @else @php($active = ' ') @endif                            
-                                        <div class="carousel-item {{ $active }} "><img class="d-block w-100 "  src="{{ $storage->url('public/small_ads/'.$photo->name.'_kw.jpg') }}" alt=" {{ $content->name }} " ></div>
+                                        <div class="carousel-item {{ $active }} "><img class="d-block w-100 "  src="{{ $storage->url('public/small_ads/'.$photo->name.'_d.jpg') }}" alt=" {{ $content->name }} " ></div>
                                 @endforeach    
 
                             </div>
@@ -52,34 +49,25 @@
                             </button>
                         </div>
                     </div>
-            <!--/.Carousel Wrapper--> 
+                     <!--/.Carousel Wrapper--> 
                     <!--/.Slides-->
                     <!--Grid column-->
-                    <div class="col-3">
+                    <div class="col-md-4">
                         <ul class="list-unstyled">
                                          
                             <li class="text-end"><small><strong>{{  $content->date_start  }}</strong></small></li>            
-                            <li class="text-end"><small>dodał: <strong>{{  $content->User->name }} </strong></small></li>
-
-                            <li><h5 class="text-center">Cena <strong class="text-success">{{ $content->price }} </strong> pln</h5></li>
-
+                            <li class="text-end"><small>od: <strong>{{  $content->User->name }} </strong></small></li>
+                            <li><h5 class="text-end">Cena <strong class="text-success">{{ $content->price }} </strong> pln</h5></li>
                             <li class="text-end"><small>tel: <strong>{{  $content->contact_phone ?? 'nie podano' }} </strong></small></li>
-                            <li class="text-end"><small>mail: <strong>{{  $content->contact_email ?? 'nie podano'  }} </strong></small></li>   
-                            
-                            
-
-                    </ul>                            
-                        
-                                                     
+                            <li class="text-end"><small>mail: <strong>{{  $content->contact_email ?? 'nie podano'  }} </strong></small></li>                               
+                        </ul>                        
                     </div>
                 </div>
-                <div class="row mt-2">
-                    <div class="col-12">
-                        <label ><strong>Informacje</strong></label>
-                        <p class="lead"><strong>{{ $content->lead }}.</strong></p>   
-                        <label ><strong>Informacje szczegółowe</strong></label>
-                        <p class="stopoverflow">{{ $content->description }}</p>
-                    </div>
+                <div class="row mt-2">                    
+                    <label ><strong>Informacje podstawowe</strong></label>
+                    <p class="lead"><strong>{{ $content->lead }}.</strong></p>   
+                    <label ><strong>Informacje szczegółowe</strong></label>
+                    <p class="stopoverflow">{{ $content->description }}</p>                    
                 </div>
             </div>
 
--- resources/views/smallAds/ListByCategories.blade.php
+++ resources/views/smallAds/ListByCategories.blade.php
@@ -2,8 +2,6 @@
 @extends('layouts.app')
 
 @section('content')
-
-
 <div class="container">
     <nav class="card p-3 pb-0 mb-3 bg-white" aria-label="breadcrumb">
         <ol class="breadcrumb" >
@@ -13,8 +11,8 @@
         </ol>
     </nav> 
     <div class="row">
-        <div class="col-3">
-            <div class="accordion  bg-white" id="menu_boczne">
+        <div class="col-md-3">
+            <div class="accordion bg-white" id="menu_boczne">
                 <div class="accordion-item">                    
                     <button class="list-group-item list-group-item-action active" type="button"  >
                         <a href="/drobne/" class="text-white"><strong>KATEGORIA</strong></a>
@@ -25,7 +23,7 @@
                 
                     <div class="accordion-item">
                         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#podkategorie_{{ $category->id }}" aria-expanded="true" aria-controls="podkategorie_{{ $category->id }}">
-                            <div class="ms-2 me-auto">
+                            <div class="me-auto">
                                 <div class="fw-bold"> {{ $category->name }}</div>
                                     {{ $category->description }}
                             </div>
@@ -51,87 +49,73 @@
             </div>
         </div>
 
-    <div class="col-7 p-0">
-
-
- 
-        @foreach($contents as $content)
-
-            
-    <div class="card m-1">
-    @php $highlighted = 'style=background-color:'.$content->highlighted.';' @endphp
-        <div class="card-body pt-1" {{ $highlighted }}>  
-            
-        <div class="col-xl-12 pt-0 ">                 
-            <p class="text-end mb-0"> <small class="text-muted">{{  $content->date_start  }}</small></p>            
-        </div>
-        <div class="col-xl-12"><h5 class="card-title"> <strong><a href="{{ route('SmallAdsContentsById', ['categories'=> $content->SmallAdsCategories->link,'subcategories'=> $content->SmallAdsSubCategories->link,'id' => $content->id ]) }}">{{ $content->name }} </a></strong></h5></div> 
-            <div class="card-text black-text">                
-                <div class="row">
-                    <div class="col-3">
-                        <div id="carousel_{{ $content->id }}" class="carousel slide" data-bs-ride="carousel">
-                            <div class="carousel-inner">
-
-                                @php
-                                    $active = null;
-                                    $count = 0;
-                                    $HTML_PHOTO ='';
-                                    $HTML_THUMBS ='';
-                                @endphp
-
-                                @foreach ($content->photos as $photo)
-                                    @if ($active==null) @php($active = 'active') @else @php($active = ' ') @endif                            
-                                        <div class="carousel-item {{ $active }} "><img class="d-block w-100 "  src="{{ $storage->url('public/small_ads/'.$photo->name.'_kw.jpg') }}" alt=" {{ $content->name }} " ></div>
-                                @endforeach    
-
-                            </div>
-                            <button class="carousel-control-prev" type="button" data-bs-target="#carousel_{{ $content->id }}"  data-bs-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Poprzednie</span>
-                            </button>
-                            <button class="carousel-control-next" type="button" data-bs-target="#carousel_{{ $content->id }}"  data-bs-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Następne</span>
-                            </button>
+        <div class="col-md-7 p-0"> 
+            @foreach($contents as $content)
+                <div class="card m-1">
+                    @php $highlighted = 'style=background-color:'.$content->highlighted.';' @endphp
+                    <div class="card-body pt-1" {{ $highlighted }}>              
+                        <div class="col-xl-12 pt-0 ">                 
+                            <p class="text-end mb-0"> <small class="text-muted">{{  $content->date_start  }}</small></p>            
                         </div>
-                    </div>
-            <!--/.Carousel Wrapper--> 
-                    <!--/.Slides-->
-                    <!--Grid column-->
-                    <div class="col-9">
-                            <!--Excerpt-->                        
-                            <h4 class="text-right">Cena: <strong>{{ $content->price }} </strong> pln</h4>
-                            <p class="lead"><strong>{{ $content->lead }}.</strong></p>                            
+                        <div class="col-xl-12">
+                            <h5 class="card-title"><strong><a href="{{ route('SmallAdsContentsById', ['categories'=> $content->SmallAdsCategories->link,'subcategories'=> $content->SmallAdsSubCategories->link,'id' => $content->id ]) }}">{{ $content->name }} </a></strong></h5>
+                        </div> 
+                        <div class="card-text black-text">                
+                            <div class="row">
+                                <div class="col-3">
+                                    <div id="carousel_{{ $content->id }}" class="carousel slide" data-bs-ride="carousel">
+                                        <div class="carousel-inner">
+                                            @php
+                                                $active = null;
+                                                $count = 0;
+                                                $HTML_PHOTO ='';
+                                                $HTML_THUMBS ='';
+                                            @endphp
+                                            @foreach ($content->photos as $photo)
+                                                @if ($active==null) @php($active = 'active') @else @php($active = ' ') @endif                            
+                                                    <div class="carousel-item {{ $active }} "><img class="d-block w-100 "  src="{{ $storage->url('public/small_ads/'.$photo->name.'_kw.jpg') }}" alt=" {{ $content->name }} " ></div>
+                                            @endforeach    
+                                        </div>
+                                        <button class="carousel-control-prev" type="button" data-bs-target="#carousel_{{ $content->id }}"  data-bs-slide="prev">
+                                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                            <span class="visually-hidden">Poprzednie</span>
+                                        </button>
+                                        <button class="carousel-control-next" type="button" data-bs-target="#carousel_{{ $content->id }}"  data-bs-slide="next">
+                                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                            <span class="visually-hidden">Następne</span>
+                                        </button>
+                                    </div>
+                                </div>
+                            <!--/.Carousel Wrapper--> 
+                                <!--/.Slides-->
+                                <!--Grid column-->
+                                <div class="col-9">
+                                        <!--Excerpt-->                        
+                                        <h4 class="text-right">Cena: <strong>{{ $content->price }} </strong> pln</h4>
+                                        <p class="lead"><strong>{{ $content->lead }}.</strong></p>                            
+                                </div>
+                            </div>
+                        
+                            <div class="row text-end"> 
+                                <form action="{{ route('SmallAdsContentsById', ['categories'=> $content->SmallAdsCategories->link,'subcategories'=> $content->SmallAdsSubCategories->link,'id' => $content->id ]) }}" method="get" enctype="text/plain"><div>
+                                    <button type="submit" class="btn btn-primary">{{ __('więcej informacji') }}</button>
+                                </form>
+                            </div>
+                        </div>
                     </div>
                 </div>
-               
-                <div class="row">  
-                    <div class="col-9 mb-3">
-                    </div>
-                    <div class="mb-3">
-                        <div class="offset-lg-9 offset-sm-4">
-                            <form action="{{ route('SmallAdsContentsById', ['categories'=> $content->SmallAdsCategories->link,'subcategories'=> $content->SmallAdsSubCategories->link,'id' => $content->id ]) }}" method="get" enctype="text/plain"><div>
-                            
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('więcej informacji') }}
-                            </button>
-                            </form>
-                        </div>
-                    </div>
             </div>
-            </div>
+            @endforeach
+        
+            <div class="row p-0"> 
+                {{ $contents->links() }}
             </div>
         </div>
-    </div>
-
-        @endforeach
-    <div class="row mt-2">
-        {{ $contents->links() }}
-    </div>
 
 
-        </div>
-        <div class="col-2">
+
+  
+        <div class="col-md-2">
 
         @include('addons.2col')  
         
